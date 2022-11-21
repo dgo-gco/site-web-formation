162a92a76488ed0c1f526912ced597ae4ca9a8ac
Diego <110729317+dgo-gco@users.noreply.github.com>
2022-11-21 19:23:01 +0100

    mobile menu now closes when clicking

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let menu = document.querySelector('.bars-menu');
 let nav = document.querySelector('.menu');
 
+const acceuil = document.querySelector('#top');
+const portfolio = document.querySelector('#applications');
+const github = document.querySelector('#github');
+const details = document.querySelector('#details');
+
+const menuBtn = document.querySelector('.menu');
+
 menu.addEventListener('click', function(){
+    nav.classList.toggle('mobile-menu');
+})
+
+menuBtn.addEventListener('click', function(){
     nav.classList.toggle('mobile-menu');
 })
